273fd74e6ade1a06a01a66d9e3c2fa57ebf3cac9
jellehuismann <jellehuisman@outlook.com>
2020-04-30 22:55:46 +0200

    supress mesage

--- Playbooks/getvlanvswitch.ps1
+++ Playbooks/getvlanvswitch.ps1
@@ -5,6 +5,7 @@ param
 )
 
 Install-Module vmware.powercli -Force
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
 connect-VIServer -Server vcenter.local -User "$usernamevcenter" -Password "$passwordvcenter" | Out-Null
 $vlan=Get-VirtualSwitch -Name DSwitch | Get-VirtualPortGroup | `
 Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
