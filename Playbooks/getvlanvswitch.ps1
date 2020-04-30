6f6b21beeac24be1115e6eec8f790dbc4a479e59
jellehuismann <jellehuisman@outlook.com>
2020-04-30 18:14:13 +0200

    vcenter pass change

--- Playbooks/getvlanvswitch.ps1
+++ Playbooks/getvlanvswitch.ps1
@@ -1,4 +1,4 @@
-connect-VIServer -Server vcenter.local -User {{ vcenter_user }} -Password {{ vcenter_pass }} | Out-Null
+connect-VIServer -Server vcenter.local -User {{ username.vcenter }} -Password {{ password.vcenter }} | Out-Null
 $vlan=Get-VirtualSwitch -Name DSwitch | Get-VirtualPortGroup | `
 Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
 $vlan | Out-File -filePath vlanresult.json
