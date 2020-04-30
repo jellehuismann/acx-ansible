f277067f0063a7bb1b6fcb1cde5d4368ad1f18c7
jellehuismann <jellehuisman@outlook.com>
2020-05-01 00:00:51 +0200

    change ps script

--- Playbooks/getvlanvswitch.ps1
+++ Playbooks/getvlanvswitch.ps1
@@ -9,4 +9,5 @@ Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false
 connect-VIServer -Server vcenter.local -User "$usernamevcenter" -Password "$passwordvcenter" | Out-Null
 $vlan=Get-VirtualSwitch -Name DSwitch | Get-VirtualPortGroup | `
 Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
-ConvertTo-Json -InputObject $vlan
+$format=$vlan | Format-List VLANId
+ConvertTo-Json -InputObject $format
