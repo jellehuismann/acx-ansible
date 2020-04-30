d737d8e6f1f2569f6554dae983411f4cb5bf626c
jellehuismann <jellehuisman@outlook.com>
2020-04-30 19:09:30 +0200

    Change vcenter.yml

--- Playbooks/getvlanvswitch.ps1
+++ Playbooks/getvlanvswitch.ps1
@@ -4,7 +4,7 @@ param
         $passwordvcenter
 )
 
-connect-VIServer -Server vcenter.local -User $usernamevcenter -Password $passwordvcenter | Out-Null
+connect-VIServer -Server vcenter.local -User "$usernamevcenter" -Password "$passwordvcenter" | Out-Null
 $vlan=Get-VirtualSwitch -Name DSwitch | Get-VirtualPortGroup | `
 Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
 $vlan.VLANId | Out-File -filePath vlanresult.json
