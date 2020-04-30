eb26efb2ce7e26e6109f7696dad4dbb47cbfc95f
jellehuismann <jellehuisman@outlook.com>
2020-05-01 00:42:25 +0200

    change values

--- Playbooks/getvlanvswitch.ps1
+++ Playbooks/getvlanvswitch.ps1
@@ -5,10 +5,10 @@ param
 )
 
 Install-Module vmware.powercli -Force
-Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
 connect-VIServer -Server vcenter.local -User "$usernamevcenter" -Password "$passwordvcenter" | Out-Null
-$vlan=Get-VirtualSwitch -Name DSwitch | Get-VirtualPortGroup | `
-Select @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
-$result=ConvertTo-Json -InputObject $vlan
+$vlan=Get-VDSwitch -Name DSwitch | Get-VDPortgroup | `
+Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
+$vlan.VLANId | Out-File -filePath vlanresult.json
+$result=$vlan.VLANId
 
-return $result
+Write-Output $result
