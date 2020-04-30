0cee4a66ae48afd9acdb2287ab3ba2720201a273
jellehuismann <jellehuisman@outlook.com>
2020-04-30 22:19:08 +0200

    return vlan

--- Playbooks/getvlanvswitch.ps1
+++ Playbooks/getvlanvswitch.ps1
@@ -8,4 +8,6 @@ Install-Module vmware.powercli -Force
 connect-VIServer -Server vcenter.local -User "$usernamevcenter" -Password "$passwordvcenter" | Out-Null
 $vlan=Get-VirtualSwitch -Name DSwitch | Get-VirtualPortGroup | `
 Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
-$vlan.VLANId | Out-File -filePath vlanresult.json
+$id=$vlan.VLANId
+
+return $id
