3c450f522f752c2217a6f9718651b0bd63f4a189
jellehuismann <jellehuisman@outlook.com>
2020-04-30 22:45:19 +0200

    Convert to Json

--- Playbooks/getvlanvswitch.ps1
+++ Playbooks/getvlanvswitch.ps1
@@ -8,6 +8,4 @@ Install-Module vmware.powercli -Force
 connect-VIServer -Server vcenter.local -User "$usernamevcenter" -Password "$passwordvcenter" | Out-Null
 $vlan=Get-VirtualSwitch -Name DSwitch | Get-VirtualPortGroup | `
 Select Name, @{N="VLANId";E={$_.Extensiondata.Config.DefaultPortCOnfig.Vlan.VlanId}}
-$id=$vlan.VLANId
-
-return $id
+ConvertTo-Json -InputObject $vlan
